a9456899c318b41f56a59a9e50d50ef6f0225dc4
FrostShock <46273569+FrostShock@users.noreply.github.com>
2021-10-19 18:15:12 +0200

    Add files via upload

--- myDPS.lua
+++ myDPS.lua
@@ -8,6 +8,11 @@ exampleFrame:RegisterEvent("CHAT_MSG_SPELL_SELF_DAMAGE")
 exampleFrame:SetScript("OnEvent", function()
   if string.find(arg1, "Fireball") then
     _,_,exampleDamage = string.find(arg1, "(%d+)")
-    DEFAULT_CHAT_FRAME:AddMessage(exampleDamage)
+    dmg = tonumber(exampleDamage)
+    mydps = mydps + dmg
+--    DEFAULT_CHAT_FRAME:AddMessage(exampleDamage)
+    DEFAULT_CHAT_FRAME:AddMessage(mydps)
   end
 end)
+
+mydps=0
