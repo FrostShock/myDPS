6fed8e74af7428c70d3cfab5eb5dc652254f39e8
FrostShock <46273569+FrostShock@users.noreply.github.com>
2021-10-19 18:51:28 +0200

    Update myDPS.lua

--- myDPS.lua
+++ myDPS.lua
@@ -1,16 +1,16 @@
-local exampleFrame = CreateFrame("Frame", "lockinfoexampleFrame", UIParent)
+local myDPSFrame = CreateFrame("Frame", "lockinfomyDPSFrame", UIParent)
 
-exampleFrame:SetWidth(100)
-exampleFrame:SetHeight(100)
-exampleFrame:SetPoint("CENTER", UIParent)
-exampleFrame:RegisterEvent("CHAT_MSG_SPELL_SELF_DAMAGE")
+myDPSFrame:SetWidth(100)
+myDPSFrame:SetHeight(100)
+myDPSFrame:SetPoint("CENTER", UIParent)
+myDPSFrame:RegisterEvent("CHAT_MSG_SPELL_SELF_DAMAGE")
 
-exampleFrame:SetScript("OnEvent", function()
+myDPSFrame:SetScript("OnEvent", function()
   if string.find(arg1, "Fireball") then
-    _,_,exampleDamage = string.find(arg1, "(%d+)")
-    dmg = tonumber(exampleDamage)
+    _,_,sdmg = string.find(arg1, "(%d+)")
+    dmg = tonumber(sdmg)
     mydps = mydps + dmg
---    DEFAULT_CHAT_FRAME:AddMessage(exampleDamage)
+--    DEFAULT_CHAT_FRAME:AddMessage(sdmg)
     DEFAULT_CHAT_FRAME:AddMessage(mydps)
   end
 end)
