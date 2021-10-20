d513b152112bca841a24ffac5e68c962bb516ed3
FrostShock <46273569+FrostShock@users.noreply.github.com>
2021-10-20 13:12:13 +0200

    Update myDPS.lua

--- myDPS.lua
+++ myDPS.lua
@@ -2,38 +2,75 @@
 
 local myDPSFrame = CreateFrame("Frame", "myDPSFrame", UIParent)
 
-myDPSFrame:SetWidth(300)
-myDPSFrame:SetHeight(300)
+myDPSFrame:SetWidth(200)
+myDPSFrame:SetHeight(200)
 myDPSFrame:SetPoint("CENTER", UIParent)
-myDPSFrame:SetBackdropColor(0.7, 0.7, 0.7, 0.7)
-myDPSFrame:RegisterEvent("CHAT_MSG_SPELL_SELF_DAMAGE")
-myDPSFrame:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_CREATURE_DAMAGE")
-myDPSFrame:RegisterEvent("CHAT_MSG_COMBAT_SELF_HITS")
-myDPSFrame:RegisterEvent("PLAYER_REGEN_ENABLED")
+myDPSFrame:EnableMouse(true)
+myDPSFrame:SetMovable(true)
 
-myDPSFrame:SetScript("OnEvent", function()
+myDPSFrame:SetScript("OnMouseDown", function()
+  if arg1 == "LeftButton" then
+    this:StartMoving()
+  end
+end)
+
+myDPSFrame:SetScript("OnMouseUp", function()
+  if arg1 == "LeftButton" then
+    this:StopMovingOrSizing()
+  end
+end)
+
+-- local tex = myDPSFrame:CreateTexture(nil, "BACKGROUND")
+-- tex:SetAllPoints()
+-- tex:SetColorTexture(1, 1, 1, 0.5)
+
+-----------------------------------------------------
+
+local textFrame = CreateFrame("Frame", nil, UIParent)
+textFrame:SetWidth(1)
+textFrame:SetHeight(1)
+textFrame:SetPoint("CENTER", myDPSFrame)
+textFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+textFrame:RegisterEvent("CHAT_MSG_SPELL_SELF_DAMAGE")
+textFrame:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_CREATURE_DAMAGE")
+textFrame:RegisterEvent("CHAT_MSG_COMBAT_SELF_HITS")
+textFrame:RegisterEvent("PLAYER_REGEN_ENABLED")
+
+-----------------------------------------------------
+
+local textFrameText = textFrame:CreateFontString()
+textFrameText:SetPoint("CENTER", textFrame)
+textFrameText:SetFont("Interface\\AddOns\\myDPS\\Fonts\\Ubuntu-R.ttf", 14, "OUTLINE")
+textFrameText:SetTextColor(0.1490, 0.5451, 0.8235)
+
+-----------------------------------------------------
+
+textFrame:SetScript("OnEvent", function()
 --  DEFAULT_CHAT_FRAME:AddMessage(event.." - "..type(event))
 --  DEFAULT_CHAT_FRAME:AddMessage(arg1)
 --  if (event == "CHAT_MSG_SPELL_SELF_DAMAGE") or (event == "CHAT_MSG_SPELL_PERIODIC_CREATURE_DAMAGE") or (event == "CHAT_MSG_COMBAT_SELF_HITS") then
-  if not (event == "PLAYER_REGEN_ENABLED") then
-    if (string.find(arg1, "Your ") and string.find(arg1, " hits ")) or string.find(arg1, "You hit ") or (string.find(arg1, " suffers ") and string.find(arg1, " from your ")) then
+  if event == "PLAYER_REGEN_ENABLED" then
+    dpsMessage = "|cffff0000Total damage in the last fight: "
+    if dpsdetails then dpsMessage = dpsMessage..smydps.." = " end
+    dpsMessage = dpsMessage..tostring(mydps)
+    DEFAULT_CHAT_FRAME:AddMessage(dpsMessage)
+    textFrameText:SetText("Damage: "..dmg.."\nTotal: "..mydps)
+    mydps = 0
+    smydps = ""
+  elseif not (event == "PLAYER_ENTERING_WORLD") then
+    if (string.find(arg1, "Your ") and (string.find(arg1, " hits ") or string.find(arg1, " crits "))) or string.find(arg1, "You hit ") or string.find(arg1, "You crit ") or (string.find(arg1, " suffers ") and string.find(arg1, " from your ")) then
       _,_,sdmg = string.find(arg1, "(%d+)")
       dmg = tonumber(sdmg)
       mydps = mydps + dmg
       if not (smydps == "") then smydps = smydps.." + " end
       smydps = smydps..sdmg
+      textFrameText:SetText("Damage: "..dmg.."\nTotal: "..mydps)
 --      DEFAULT_CHAT_FRAME:AddMessage(dmg)
     end
-  else
-    dpsMessage = "|cffff0000Total damage in the last fight: "
-    if dpsdetails then dpsMessage = dpsMessage..smydps.." = " end
-    dpsMessage = dpsMessage..tostring(mydps)
-    DEFAULT_CHAT_FRAME:AddMessage(dpsMessage)
-    mydps = 0
-    smydps = ""
   end
 end)
 
+dmg=0
 mydps=0
 smydps=""
 
